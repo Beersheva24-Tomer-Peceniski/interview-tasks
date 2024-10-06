6153fd9180d7d28c0dbfcfe7c9984d4fdae5c5a5
Tomer Peceniski <tomerpeceniski@gmail.com>
2024-10-06 19:43:57 +0300

    HW32 resolution of ConnectionPool

--- src/main/java/telran/interview/ConnectionPool.java
+++ src/main/java/telran/interview/ConnectionPool.java
@@ -1,7 +1,12 @@
 package telran.interview;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.Set;
+
 public class ConnectionPool {
-    // TODO work out data structure
+    // work out data structure
     // Connection pool comprises of some number of connections
     // It canot contains more than the predefined number of connection
     // You should define some Connection Pool policy so that if number of
@@ -10,20 +15,33 @@ public class ConnectionPool {
     // connection
     // Policy should take in consideration the order of adding connections in pool
     // and using connection
+    private LinkedHashMap<String, Connection> map;
+    
     public ConnectionPool(int size) {
-        // TODO
+        map = new LinkedHashMap<>(size * 2, 0.75f, true) {
+            @Override
+            protected boolean removeEldestEntry(Map.Entry<String, Connection> entry) {
+                return size() > size;
+            };
+        };
     }
 
     public void addConnection(Connection connection) {
-        // TODO
-        // if the connection already exists in the pool the IllegalStateException should
-        // be thrown
+        if(map.containsKey(connection.connectionId())) {
+            throw new IllegalStateException();
+        }
+        map.put(connection.connectionId(), connection);
     }
 
     public Connection getConnection(String connectionId) {
-        // TODO
-        // If connection with a given ID doesn't exist the NoSuchElementException should
-        // be thrown
-        return null;
+        Connection result = map.get(connectionId);
+        if (result == null) {
+            throw new NoSuchElementException();
+        }
+        return result;
+    }
+
+    public Set<String> idSet() {
+        return map.keySet();
     }
 }
--- src/test/java/telran/interview/ConnectionPoolTest.java
+++ src/test/java/telran/interview/ConnectionPoolTest.java
@@ -1,6 +1,40 @@
 package telran.interview;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
 public class ConnectionPoolTest {
-    // TODo
-    // should contain the tests reflecting your understanding what should be done
+    ConnectionPool cp = new ConnectionPool(3);
+    
+    void setUp() {
+        cp.addConnection(new Connection("id1"));
+        cp.addConnection(new Connection("id2"));
+        cp.addConnection(new Connection("id3"));
+    }
+
+    @Test 
+    void firstOrderTest() {
+        setUp();
+        String[] expected = {"id1", "id2", "id3"};
+        assertArrayEquals(expected, cp.idSet().toArray(String[]::new));
+    }
+
+    @Test 
+    void addingErrorTest() {
+        setUp();
+        assertThrowsExactly(IllegalStateException.class, () -> cp.addConnection(new Connection("id1")));
+    }
+
+    @Test
+    void newOrderTest() {
+        setUp();
+        cp.addConnection(new Connection("id4"));
+        String[] expected = {"id2", "id3", "id4"};
+        assertArrayEquals(expected, cp.idSet().toArray(String[]::new));
+        cp.getConnection("id2");
+        cp.addConnection(new Connection("id5"));
+        String[] expected2 = {"id4", "id2", "id5"};
+        assertArrayEquals(expected2, cp.idSet().toArray(String[]::new));
+    }
 }
